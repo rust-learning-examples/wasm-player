cdc17eaa20a04300d4c5a3cab1e93707e6cbcdac
三木 <578595193@qq.com>
2022-04-06 21:36:53 +0800

    FEAT: parse m3u8 url

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use m3u8_rs::Playlist;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -14,6 +15,22 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-player!");
+pub async fn greet() -> Result<JsValue, JsValue> {
+    // https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8
+    // https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8
+    // https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
+    let res = match reqwest::Client::new().get("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8").send().await {
+        Ok(res) => res,
+        Err(_) => return Err("网络错误".into()),
+    };
+    let m3u8_text = match res.text().await {
+        Ok(text) => text,
+        Err(_) => return Err("解析错误".into())
+    };
+
+    match m3u8_rs::parse_playlist_res(m3u8_text.as_bytes()) {
+        Ok(Playlist::MasterPlaylist(pl)) => Ok(format!("{:#?}", pl).into()),
+        Ok(Playlist::MediaPlaylist(pl)) => Ok(format!("{:#?}", pl).into()),
+        Err(e) => Err(format!("{:?}", e).into()),
+    }
 }
--- www/index.js
+++ www/index.js
@@ -2,5 +2,14 @@
 // wasm.greet();
 
 import * as wasm from 'wasm-player'
-wasm.greet();
+
+wasm.greet().then(txt => {
+    console.log(11, txt)
+    const pre = document.createElement('pre')
+    pre.style = 'font-size: 12px;'
+    pre.innerText = txt
+    document.body.appendChild(pre)
+}).catch(e => {
+    console.log(12, e)
+})
 
